ae824c2b5e7a62b6e24ddd47b18146b12b5472a9
viktor-drok <vityadrok@gmail.com>
2023-08-29 21:59:04 +0300

    add progress bar on day button

--- components/CustomButton.jsx
+++ components/CustomButton.jsx
@@ -1,6 +1,8 @@
-const CustomButton = ({ className = "", children }) => {
+const CustomButton = ({ className = "", children, onDragEnter, style }) => {
 	return (
 		<div
+			style={style}
+			onDragEnter={onDragEnter}
 			className={`${className} flex justify-center items-center h-[50px] text-[22px] border-none rounded-[8px] w-[254px] cursor-pointer`}
 		>
 			{children}
--- components/ToDosByDay.jsx
+++ components/ToDosByDay.jsx
@@ -6,20 +6,21 @@ const CustomButton = dynamic(() => import("./CustomButton"), { ssr: false });
 
 import Link from "next/link";
 import { useTodos } from "@/state/todos";
+import { useRouter } from "next/navigation";
 
 const ToDosByDay = () => {
 	const todos = useTodos(state => state.todos);
+
+	const router = useRouter();
 
 	const filteredByDay = day => todos.filter(todo => todo.day.toLowerCase() === day.toLowerCase());
 
 	const filteredByDayAndCompleted = day => filteredByDay(day).filter(todo => todo.completed === true);
 
 	const styleButton = day => {
-		const gradient =
-			Math.round((filteredByDayAndCompleted(day).length * 100) / filteredByDay(day).length) -
-			(Math.round((filteredByDayAndCompleted(day).length * 100) / filteredByDay(day).length) % 5);
+		const gradient = (filteredByDayAndCompleted(day).length * 100) / filteredByDay(day).length;
 
-		return `justify-between px-4 bg-gradient-to-r from-[#7FDB88] from-${gradient}% to-[#FFD7A7] to-${gradient}%`;
+		return gradient;
 	};
 
 	//     `bg-gradient-to-r from-[#7FDB88] from-30% to-[#FFD7A7] to-30% flex justify-between px-4`
@@ -33,17 +34,30 @@ const ToDosByDay = () => {
 					href={`/${day.toLowerCase()}`}
 					key={day}
 				>
-					<CustomButton className={styleButton(day)}>
+					<CustomButton
+						style={{
+							background: `linear-gradient(90deg, rgba(127, 219, 136, 1) ${styleButton(
+								day,
+							)}%, rgba(255, 215, 167, 1) ${styleButton(day)}%)`,
+							// transition: "all 0.5s ease-in-out",
+						}}
+						className="justify-between px-4 bg-details-bg transition-all"
+						onDragEnter={e => {
+							e.preventDefault();
+							router.push(`/${day.toLowerCase()}`);
+						}}
+					>
 						{day}
-						<span>{filteredByDay(day).length}</span>
-						<span>
-							{(
-								`${
-									Math.round((filteredByDayAndCompleted(day).length * 100) / filteredByDay(day).length) -
-									(Math.round((filteredByDayAndCompleted(day).length * 100) / filteredByDay(day).length) % 5)
-								}` + "%"
-							).replace('"', "")}
-						</span>
+						<div className="flex gap-4">
+							<span>{filteredByDay(day).length}</span>
+							<span>
+								{`${
+									Number.isNaN((filteredByDayAndCompleted(day).length * 100) / filteredByDay(day).length)
+										? 0
+										: Math.round((filteredByDayAndCompleted(day).length * 100) / filteredByDay(day).length)
+								}%`}
+							</span>
+						</div>
 						{/* {styleButton(day)} */}
 					</CustomButton>
 				</Link>
